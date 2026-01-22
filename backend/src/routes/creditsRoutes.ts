45e8435bbc919fc320cc69afe99bede81bb9f591
Ashish Dhiman <ashishdhiman@Ashishs-MacBook-Air.local>
2026-01-22 11:00:44 +0530

    fix: Fix syntax error in creditsRoutes.ts imports

--- backend/src/routes/creditsRoutes.ts
+++ backend/src/routes/creditsRoutes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client'; \nimport logger from '../utils/logger';
+import { PrismaClient } from '@prisma/client';
+import logger from '../utils/logger';
 
 const router = express.Router();
 const prisma = new PrismaClient();
